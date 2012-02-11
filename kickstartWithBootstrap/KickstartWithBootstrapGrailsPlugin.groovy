049cd5b1b40291ba14ea6eec7de393393a1df127
rech <rech@Desktop>
2012-02-11 12:05:17 +0100

    Improvement of error.gsp

--- kickstartWithBootstrap/KickstartWithBootstrapGrailsPlugin.groovy
+++ kickstartWithBootstrap/KickstartWithBootstrapGrailsPlugin.groovy
@@ -1,13 +1,13 @@
 class KickstartWithBootstrapGrailsPlugin {
     // the plugin version
-    def version = "0.5.2"
+    def version = "0.5.3"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
-            "grails-app/views/error.gsp"
+//            "grails-app/views/error.gsp"
     ]
 	
 	def license = "APACHE"
