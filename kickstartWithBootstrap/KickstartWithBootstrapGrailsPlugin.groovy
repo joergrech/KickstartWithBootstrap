c6466793bcca77079350bf20f04269cba2d4835a
rech <rech@Desktop>
2012-02-18 10:42:04 +0100

    Added script to calculate or retrieve build numbers. Updated Documentation

--- kickstartWithBootstrap/KickstartWithBootstrapGrailsPlugin.groovy
+++ kickstartWithBootstrap/KickstartWithBootstrapGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class KickstartWithBootstrapGrailsPlugin {
     // the plugin version
-    def version = "0.5.6"
+    def version = "0.5.7"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.3 > *"
     // the other plugins this plugin depends on
