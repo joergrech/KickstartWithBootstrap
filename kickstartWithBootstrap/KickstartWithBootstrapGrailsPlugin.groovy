d3a2820b41e0a3a2e566073e4a89447f5872b951
Jörg Rech <joerg.rech@gmail.com>
2012-10-19 18:05:31 +0200

    Corrected version number

--- kickstartWithBootstrap/KickstartWithBootstrapGrailsPlugin.groovy
+++ kickstartWithBootstrap/KickstartWithBootstrapGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class KickstartWithBootstrapGrailsPlugin {
     // the plugin version
-    def version = "0.7.1"
+    def version = "0.7.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0.0 > *"
     // the other plugins this plugin depends on
