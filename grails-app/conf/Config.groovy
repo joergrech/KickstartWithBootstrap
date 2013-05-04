448d48b2479322bccc9d029bb2457998cf378bbb
Jörg Rech <joerg.rech@gmail.com>
2013-05-04 09:31:37 +0200

    Fix for Issue #38

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -29,3 +29,5 @@ log4j = {
 	info	'grails.app.filters'
 }
 grails.config.defaults.locations = [KickstartResources]
+grails.views.default.codec="none" // none, html, base64
+grails.views.gsp.encoding="UTF-8"
